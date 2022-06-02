8d9a85dd75c9dd94434b2e115447e1884a74af38
naryeong-ko <naryung26@gmail.com>
2022-06-02 15:49:08 +0900

    220602_solve : 예산_ko

--- 0602/Programmers/Lv1/예산_ko.java
+++ 0602/Programmers/Lv1/예산_ko.java
@@ -1,1 +1,19 @@
+import java.util.Arrays;
 
+class Solution {
+
+	public int solution(int[] d, int budget) {
+		Arrays.sort(d);
+		int len = d.length;
+		int ans = 0;
+
+		for (; ans < len; ans++) {
+			budget -= d[ans];
+			if (budget < 0)
+				break;
+		}
+
+		return ans;
+	}
+
+}

f8eb96a0521cbc470aa259c96afe99f4b740229e
naryeong-ko <naryung26@gmail.com>
2022-06-02 15:49:18 +0900

    220602_solve : 폰켓몬_ko

--- 0602/Programmers/Lv1/폰켓몬_ko.java
+++ 0602/Programmers/Lv1/폰켓몬_ko.java
@@ -1,1 +1,12 @@
+import java.util.HashSet;
 
+class Solution {
+	public int solution(int[] nums) {
+		int len = nums.length;
+		HashSet<Integer> set = new HashSet<>();
+		for (int i = 0; i < len; i++)
+			set.add(nums[i]);
+
+		return (set.size() >= len / 2) ? len / 2 : set.size();
+	}
+}
